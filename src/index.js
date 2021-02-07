02e5aa1dbbe866ffe1032a328fa1262aff14ce47
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-02-08 00:46:56 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) { //проходим циклом по каждому элементу многомерного массива bracketsConfig
+      let symbolsOfSubArray = bracketsConfig[i].join('');//в переменную определяем объединенные элементы субмассива
+      if (str.includes(symbolsOfSubArray)) {//проверяем условие, если первый аргумент строка включает в себя парные символы
+          str = str.replace(symbolsOfSubArray, '');//заменяем совпадение на пустую строку
+          i = - 1;
+       }
+     }
+      return str.length === 0;
 }
+/*module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+  let objFromBracketsConfig = Object.fromEntries(bracketsConfig);
+  let strArray = str.split('');
+  for (let elementStrArray of strArray) {
+      if (stack[0] === elementStrArray) {
+          stack.pop();
+      } else if (objFromBracketsConfig[elementStrArray]) {
+          stack.push(objFromBracketsConfig[elementStrArray]);
+      } else {
+          return false;
+      }
+  }
+  return !stack.length;
+}*/
+
+  //console.log(check("|(|)",[['(', ')'], ['|', '|']]));
